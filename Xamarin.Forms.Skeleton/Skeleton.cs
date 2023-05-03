e81333d68f4444e882fe3f1f3358fb7e780f7d7b
Bryan Dominguez <jules.bryand@gmail.com>
2023-05-03 14:39:38 -0500

    Undo per-property BindableProperty regions

--- Xamarin.Forms.Skeleton/Skeleton.cs
+++ Xamarin.Forms.Skeleton/Skeleton.cs
@@ -10,130 +10,95 @@ namespace Xamarin.Forms.Skeleton
     {
         #region Public Properties
 
-        public static readonly BindableProperty MockItemTemplateProperty
-        #region
-            = BindableProperty.CreateAttached("MockItemTemplate", typeof(DataTemplate), typeof(View), defaultValue: null);
-        public static DataTemplate GetMockItemTemplate(BindableObject b) => (DataTemplate)b.GetValue(MockItemTemplateProperty);
-        public static void SetMockItemTemplate(BindableObject b, DataTemplate value) => b.SetValue(MockItemTemplateProperty, value);
-        #endregion
+        public static readonly BindableProperty MockItemTemplateProperty = BindableProperty.CreateAttached("MockItemTemplate", typeof(DataTemplate), typeof(View), defaultValue: null);
 
-        public static readonly BindableProperty MockItemNumberProperty
-        #region
-            = BindableProperty.CreateAttached("MockItemNumber", typeof(int), typeof(View), defaultValue: -1);
+        public static DataTemplate GetMockItemTemplate(BindableObject b) => (DataTemplate)b.GetValue(MockItemTemplateProperty);
+
+        public static void SetMockItemTemplate(BindableObject b, DataTemplate value) => b.SetValue(MockItemTemplateProperty, value);
+
+        public static readonly BindableProperty MockItemNumberProperty = BindableProperty.CreateAttached("MockItemNumber", typeof(int), typeof(View), defaultValue: -1);
 
         public static void SetMockItemNumber(BindableObject b, int value) => b.SetValue(MockItemNumberProperty, value);
 
         public static int GetMockItemNumber(BindableObject b) => (int)b.GetValue(MockItemNumberProperty);
-        #endregion
 
-        public static readonly BindableProperty IsParentProperty
-        #region
-            = BindableProperty.CreateAttached("IsParent", typeof(bool), typeof(View), false);
+        public static readonly BindableProperty IsParentProperty = BindableProperty.CreateAttached("IsParent", typeof(bool), typeof(View), false);
 
         public static void SetIsParent(BindableObject b, bool value) => b.SetValue(IsParentProperty, value);
 
         public static bool GetIsParent(BindableObject b) => (bool)b.GetValue(IsParentProperty);
-        #endregion
 
-        public static readonly BindableProperty IsBusyProperty
-        #region
-            = BindableProperty.CreateAttached("IsBusy", typeof(bool), typeof(View), default(bool), propertyChanged: (b, oldValue, newValue) => OnIsBusyChanged(b, (bool)newValue));
+        public static readonly BindableProperty IsBusyProperty = BindableProperty.CreateAttached("IsBusy", typeof(bool), typeof(View), default(bool), propertyChanged: (b, oldValue, newValue) => OnIsBusyChanged(b, (bool)newValue));
 
         public static void SetIsBusy(BindableObject b, bool value) => b.SetValue(IsBusyProperty, value);
 
         public static bool GetIsBusy(BindableObject b) => (bool)b.GetValue(IsBusyProperty);
-        #endregion
 
-        public static readonly BindableProperty HideProperty
-        #region
-            = BindableProperty.CreateAttached("Hide", typeof(bool), typeof(View), default(bool));
+        public static readonly BindableProperty HideProperty = BindableProperty.CreateAttached("Hide", typeof(bool), typeof(View), default(bool));
 
         public static void SetHide(BindableObject b, bool value) => b.SetValue(HideProperty, value);
 
         public static bool GetHide(BindableObject b) => (bool)b.GetValue(HideProperty);
-        #endregion
 
-        public static readonly BindableProperty BackgroundColorProperty
-        #region
-            = BindableProperty.CreateAttached("BackgroundColor", typeof(Color), typeof(View), default(Color));
+        public static readonly BindableProperty BackgroundColorProperty = BindableProperty.CreateAttached("BackgroundColor", typeof(Color), typeof(View), default(Color));
 
         public static void SetBackgroundColor(BindableObject b, Color value) => b.SetValue(BackgroundColorProperty, value);
 
         public static Color GetBackgroundColor(BindableObject b) => (Color)b.GetValue(BackgroundColorProperty);
-        #endregion
 
-        public static readonly BindableProperty AnimationProperty
-        #region
-            = BindableProperty.CreateAttached("Animation", typeof(BaseAnimation), typeof(View), null);
+        public static readonly BindableProperty AnimationProperty = BindableProperty.CreateAttached("Animation", typeof(BaseAnimation), typeof(View), null);
 
         public static void SetAnimation(BindableObject b, BaseAnimation value) => b.SetValue(AnimationProperty, value);
 
         public static BaseAnimation GetAnimation(BindableObject b) => (BaseAnimation)b.GetValue(AnimationProperty);
-        #endregion
 
         #endregion Public Properties
 
         #region Internal Properties
 
-        internal static readonly BindableProperty AnimatingProperty
-        #region
-            = BindableProperty.CreateAttached("Animating", typeof(bool), typeof(View), default(bool));
+        internal static readonly BindableProperty AnimatingProperty = BindableProperty.CreateAttached("Animating", typeof(bool), typeof(View), default(bool));
 
         internal static void SetAnimating(BindableObject b, bool value) => b.SetValue(AnimatingProperty, value);
 
         internal static bool GetAnimating(BindableObject b) => (bool)b.GetValue(AnimatingProperty);
-        #endregion
 
-        internal static readonly BindableProperty CancelAnimationProperty
-        #region
-            = BindableProperty.CreateAttached("CancelAnimation", typeof(bool), typeof(View), default(bool));
+        internal static readonly BindableProperty CancelAnimationProperty = BindableProperty.CreateAttached("CancelAnimation", typeof(bool), typeof(View), default(bool));
 
         internal static void SetCancelAnimation(BindableObject b, bool value) => b.SetValue(CancelAnimationProperty, value);
 
         internal static bool GetCancelAnimation(BindableObject b) => (bool)b.GetValue(CancelAnimationProperty);
-        #endregion
 
-        internal static readonly BindableProperty OriginalBackgroundColorProperty
-        #region
-            = BindableProperty.CreateAttached("OriginalBackgroundColor", typeof(Color), typeof(View), default(Color));
+        internal static readonly BindableProperty OriginalBackgroundColorProperty = BindableProperty.CreateAttached("OriginalBackgroundColor", typeof(Color), typeof(View), default(Color));
 
         internal static void SetOriginalBackgroundColor(BindableObject b, Color value) => b.SetValue(OriginalBackgroundColorProperty, value);
 
         internal static Color GetOriginalBackgroundColor(BindableObject b) => (Color)b.GetValue(OriginalBackgroundColorProperty);
-        #endregion
 
-        internal static readonly BindableProperty UseDynamicTextColorProperty
-        #region
-            = BindableProperty.CreateAttached("UseDynamicTextColor", typeof(bool), typeof(View), default(bool));
+        internal static readonly BindableProperty UseDynamicTextColorProperty = BindableProperty.CreateAttached("UseDynamicTextColor", typeof(bool), typeof(View), default(bool));
 
         internal static void SetUseDynamicTextColor(BindableObject b, bool value) => b.SetValue(UseDynamicTextColorProperty, value);
 
         internal static bool GetUseDynamicTextColor(BindableObject b) => (bool)b.GetValue(UseDynamicTextColorProperty);
-        #endregion
 
-        internal static readonly BindableProperty UseDynamicBackgroundColorProperty
-        #region
-            = BindableProperty.CreateAttached("UseDynamicBackground", typeof(bool), typeof(View), default(bool));
+        internal static readonly BindableProperty UseDynamicBackgroundColorProperty = BindableProperty.CreateAttached("UseDynamicBackground", typeof(bool), typeof(View), default(bool));
 
         internal static bool GetUseDynamicBackgroundColor(BindableObject b) => (bool)b.GetValue(UseDynamicBackgroundColorProperty);
 
         internal static void SetUseDynamicBackgroundColor(BindableObject b, bool value) => b.SetValue(UseDynamicBackgroundColorProperty, value);
-        #endregion
 
-        internal static readonly BindableProperty OriginalTextColorProperty
-        #region
-            = BindableProperty.CreateAttached("OriginalTextColor", typeof(Color), typeof(View), default(Color));
+        internal static readonly BindableProperty OriginalTextColorProperty = BindableProperty.CreateAttached("OriginalTextColor", typeof(Color), typeof(View), default(Color));
 
         internal static void SetOriginalTextColor(BindableObject b, Color value) => b.SetValue(OriginalTextColorProperty, value);
 
         internal static Color GetOriginalTextColor(BindableObject b) => (Color)b.GetValue(OriginalTextColorProperty);
-        #endregion
 
         #endregion Internal Properties
 
         #region Private Fields
+
         static Dictionary<Guid, DataTemplate> _internalListDataTemplates = new Dictionary<Guid, DataTemplate>();
         static Dictionary<Guid, Binding> _listViewBindings = new Dictionary<Guid, Binding>();
+
         #endregion Private Fields
 
         #region Operations
@@ -158,13 +123,10 @@ namespace Xamarin.Forms.Skeleton
             var view = (View)bindable;
             if (isBusyNewValue)
             {
-
-
                 if (GetHide(bindable))
                 {
                     ((View)bindable).IsVisible = false;
                 }
-
                 else
                 {
                     if (view is ListView listView)
@@ -184,9 +146,7 @@ namespace Xamarin.Forms.Skeleton
                     SetBackgroundColor(view);
 
                     RunAnimation(view);
-                }
-            }
-            else
+                }             }             else
             {
                 if (GetHide(bindable))
                 {
@@ -213,8 +173,7 @@ namespace Xamarin.Forms.Skeleton
                         RestoreTextColor(view);
                     }
                 }
-            }
-        }
+            }         }
 
         private static void SetLayoutChilds(Layout layout)
         {
@@ -424,5 +383,4 @@ namespace Xamarin.Forms.Skeleton
 
         #endregion Operations
     }
-
 }
